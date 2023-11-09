f0bad82ca2d413cc32e17c7883ab39983e499528
Ayodeji O <97124713+aosasona@users.noreply.github.com>
2023-11-09 07:14:51 +0000

    Began migration

--- pkg/types/admin.go
+++ pkg/types/admin.go
@@ -1,23 +1,25 @@
 package types
 
-type Admin struct {
-	// EnableAPI is a flag to enable the admin API
-	Enabled bool `json:"enabled" ts:"type:boolean"`
-
-	// Port is the port that the admin API will listen on
-	Port int `json:"port" ts:"type:number"`
-
-	// APIKey is the key that the admin API will use for authentication
-	APIKey string `json:"api_key" ts:"type:string"`
-}
+import "go.trulyao.dev/lito/ext/option"
 
 // type Admin struct {
 // 	// EnableAPI is a flag to enable the admin API
-// 	Enabled option.Bool `json:"enabled" ts:"type:boolean"`
+// 	Enabled bool `json:"enabled" ts:"type:boolean"`
 //
 // 	// Port is the port that the admin API will listen on
-// 	Port option.Int `json:"port" ts:"type:number"`
+// 	Port int `json:"port" ts:"type:number"`
 //
 // 	// APIKey is the key that the admin API will use for authentication
-// 	APIKey option.Int `json:"api_key" ts:"type:string"`
+// 	APIKey string `json:"api_key" ts:"type:string"`
 // }
+
+type Admin struct {
+	// EnableAPI is a flag to enable the admin API
+	Enabled option.Bool `json:"enabled" ts:"type:boolean"`
+
+	// Port is the port that the admin API will listen on
+	Port option.Int `json:"port" ts:"type:number"`
+
+	// APIKey is the key that the admin API will use for authentication
+	APIKey option.Int `json:"api_key" ts:"type:string"`
+}
