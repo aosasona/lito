186faa6f7068774e57a9bcc0542024618562a8f6
Ayodeji O <97124713+aosasona@users.noreply.github.com>
2024-10-04 08:24:16 +0100

    Implemented TOML storage backend and set as default

--- pkg/storage/toml.go
+++ pkg/storage/toml.go
@@ -3,7 +3,10 @@ package storage
 import (
 	"fmt"
 	"os"
+	"path/filepath"
+	"strings"
 
+	"github.com/pelletier/go-toml"
 	"go.trulyao.dev/lito/pkg/logger"
 	"go.trulyao.dev/lito/pkg/ref"
 	"go.trulyao.dev/lito/pkg/types"
@@ -43,15 +46,98 @@ func (t *TOML) Path() string {
 }
 
 func (t *TOML) Load() error {
-	panic("unimplemented")
+	if err := t.CreateIfNotExists(); err != nil {
+		return err
+	}
+
+	t.config.Lock()
+	defer t.config.Unlock()
+
+	config, err := t.read()
+	if err != nil {
+		return fmt.Errorf("failed to read config from disk: %w", err)
+	}
+
+	t.config.Update(config)
+	t.debug("successfully loaded config from disk", logger.Param{Key: "path", Value: t.Path()})
+
+	return nil
 }
 
-func (t *TOML) CreateIfNotExists(...string) error {
-	panic("unimplemented")
+func (t *TOML) CreateIfNotExists(optPath ...string) error {
+	// If an alternative path is provided use, that as our new config path and create the config if it doesn't already exist
+	if len(optPath) > 0 && strings.TrimSpace(optPath[0]) != "" {
+		if t.config.Proxy == nil {
+			t.config.Proxy = &types.DefaultProxy
+		}
+
+		t.config.Proxy.ConfigPath = ref.Ref(optPath[0])
+	}
+
+	if !t.exists() || t.isEmpty() {
+		if err := t.init(); err != nil {
+			return err
+		}
+	}
+
+	return nil
 }
 
 func (t *TOML) Persist() error {
-	panic("unimplemented")
+	t.config.Lock()
+	defer t.config.Unlock()
+
+	configBytes, err := t.config.ToTOML()
+	if err != nil {
+		return fmt.Errorf("failed to convert config to JSON: %s", err.Error())
+	}
+
+	if err = os.WriteFile(t.Path(), configBytes, os.ModePerm); err != nil {
+		return fmt.Errorf("failed to write config to disk: %s", err.Error())
+	}
+
+	t.debug("successfully persisted config to disk", logger.Param{Key: "path", Value: t.Path()})
+	return nil
+}
+
+func (t *TOML) read() (*types.Config, error) {
+	configBytes, err := os.ReadFile(t.Path())
+	if err != nil {
+		return &types.Config{}, err
+	}
+
+	config := new(types.Config)
+	if err = toml.Unmarshal(configBytes, config); err != nil {
+		return &types.Config{}, err
+	}
+
+	return config, nil
+}
+
+// init() creates the config file on disk using the current config in memory
+func (t *TOML) init() error {
+	t.config.Lock()
+	defer t.config.Unlock()
+
+	if err := os.MkdirAll(filepath.Dir(t.Path()), os.ModePerm); err != nil {
+		return err
+	}
+
+	// Protection	against overwriting existing config files
+	var (
+		configBytes []byte
+		err         error
+	)
+
+	if configBytes, err = t.config.ToTOML(); err != nil {
+		return err
+	}
+
+	if err = os.WriteFile(t.Path(), configBytes, os.ModePerm); err != nil {
+		return err
+	}
+
+	return nil
 }
 
 func (t *TOML) remove() error {
--- pkg/storage/toml_test.go
+++ pkg/storage/toml_test.go
@@ -1,1 +1,3 @@
 package storage
+
+// TODO: add tests
